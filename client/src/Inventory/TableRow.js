2cfca9f0311d9375c94159fd4ffe246ed662dccd
Jerzy <jerzy.kubisiak@gmail.com>
2023-02-16 22:36:38 +0100

    create

---
+++ client/src/Inventory/TableRow.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+export default function TableRow({ el }) {
+
+    const nav = useNavigate()
+
+    return (
+        // <Link to={`${el.serialNumber}`}>
+            <tr onClick={() => nav(`${el._id}`)}>
+                <td>{el.name}</td>
+                <td>{el.serialNumber}</td>
+                <td>{el.createdAt}</td>
+                <td>{el.owner}</td>
+            </tr>
+        // </Link>
+    )
+}
