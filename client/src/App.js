323d66003dfb9a8978b600671b39f3daa7d4c668
Jerzy <jerzy.kubisiak@gmail.com>
2023-02-16 22:35:50 +0100

    add inventory routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import DashBoard from './DashBoard';
 import EditArticle from './KnowledgeBase/EditArticle';
-import Inventory from './Inventory';
+import Inventory from './Inventory/Inventory';
 import KnowlageBase from './KnowledgeBase/KnowlageBase';
 import Navbar from './Navbar';
 import NewKnowlageBase from './KnowledgeBase/NewKnowlageBase';
 import OneArticle from './KnowledgeBase/OneArticle';
+import NewItem from './Inventory/NewItem';
+import OneInventory from './Inventory/OneInventory';
 
 //Knowledge Base
 
@@ -17,6 +19,8 @@ function App() {
         <Routes>
           <Route path='/' element={<DashBoard />} />
           <Route path='/inventory' element={<Inventory />} />
+          <Route path='/inventory/new' element={<NewItem/>} />
+          <Route path='/inventory/:id' element={<OneInventory/>} />
           <Route path='/KnowledgeBase' element={<KnowlageBase />} />
           <Route path='/KnowledgeBase/new' element={<NewKnowlageBase />} />
           <Route path='/KnowledgeBase/:id' element={<OneArticle />} />
