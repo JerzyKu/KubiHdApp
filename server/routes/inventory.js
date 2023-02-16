cd74b2f43d6eeaf3fff98a8afce91ac69dc4ea58
Jerzy <jerzy.kubisiak@gmail.com>
2023-02-16 22:36:25 +0100

    add getOne route and getItem middle

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -26,4 +26,24 @@ router.get('/', async (req, res) => {
     }
 })
 
+// Get one 
+router.get('/:id', getItem, async (req, res) => {
+    res.json(res.item)
+})
+
+async function getItem(req, res, next) {
+    let item
+    try {
+        item = await Item.findById(req.params.id)
+        if (item == null) {
+            return res.status(404).json({ message: "cannot find Item" })
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+
+    res.item = item
+    next()
+}
+
 module.exports = router
