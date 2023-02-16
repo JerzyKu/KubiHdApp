70783e3760c0e93ac62e2875b5c70a5fa9064e9e
Jerzy <jerzy.kubisiak@gmail.com>
2023-02-16 22:38:47 +0100

    create inventory page

---
+++ client/src/Inventory/Inventory.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import TableRow from './TableRow'
+import './style.css'
+import { Link } from 'react-router-dom'
+
+export default function Inventory() {
+
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3500/inventory')
+      .then(res => {
+        setItems(res.data)
+      })
+      .catch(err => {
+        console.log(err);
+        setItems(err)
+      })
+
+
+  }, [])
+
+
+  return (
+    <>
+    <Link to={`/inventory/new`}> Add Item </Link> <br />
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>serialNumber</th>
+            <th>createdAt</th>
+            <th>owner</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(el => (
+            <TableRow el={el} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  )
+}
