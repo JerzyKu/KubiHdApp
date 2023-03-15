aa6d427e1648e686bdf716494bd1496ff0f13fbc
Jerzy <jerzy.kubisiak@gmail.com>
2023-03-15 18:22:18 +0100

    some fixes

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,22 +1,21 @@
 import { useEffect, useState } from "react"
 
-function getInitialValue(key, initialValue){
-    const savedValue = localStorage.getItem(key)
+function getSavedValue(key, initialValue){
+    const savedValue = JSON.parse(localStorage.getItem(key))
     if (savedValue) return savedValue
     if (initialValue instanceof Function) return initialValue()
     return initialValue
 }
 
-export default function useLocalStorage(key, initialValue){
-    const [value, setValue] = useState(() => {
-        return getInitialValue(key, initialValue)
+export default function useLocalStorage(key, initialValue) {
+    const [value, setValue] = useState( () => {
+        return getSavedValue(key, initialValue)
     })
 
     useEffect(() => {
-      localStorage.setItem(key, value)
-      // eslint-disable-next-line
+        localStorage.setItem(key, JSON.stringify(value))
+        // eslint-disable-next-line
     }, [value])
-    
 
     return [value, setValue]
 }
