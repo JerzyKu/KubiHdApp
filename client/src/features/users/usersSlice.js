23d3e47bfb598dd7a650a8df33ac3755c483a897
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-05-05 16:46:40 +0200

    add two names

--- client/src/features/users/usersSlice.js
+++ client/src/features/users/usersSlice.js
@@ -3,7 +3,9 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = [
     {id: '0', name: 'Dude Lebowski'},
     {id: '1', name: 'Neil Young'},
-    {id: '2', name: 'Dave Gray'},
+    {id: '2', name: 'Jakub Wilczek'},
+    {id: '3', name: 'Szymon Bączyk'},
+    {id: '4', name: 'Dave Gray'},
 ]
 
 
