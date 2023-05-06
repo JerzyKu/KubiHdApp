b7468f642e39012b8a16faab049c2558a014b665
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-05-06 23:43:25 +0200

    off strict mode to prevent double sending requests

--- client/src/index.js
+++ client/src/index.js
@@ -12,13 +12,13 @@ store.dispatch(fetchUsers())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter >
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 
