d8ef3f0c130a3cd862d597a174e5382fa444e91b
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-05-06 23:44:28 +0200

    improver

--- client/src/features/posts/PostsExcerpt.js
+++ client/src/features/posts/PostsExcerpt.js
@@ -2,15 +2,19 @@ import React from 'react'
 import PostAuthor from './PostAuthor'
 import TimeAgo from './TimeAgo'
 import ReactionButtons from './ReactionButtons'
+import { Link } from 'react-router-dom'
 
 
 export default function PostsExcerpt({ post }) {
     return (
         <article>
             <h3>{post.title}</h3>
-            <PostAuthor userId={post.userId} />
-            <TimeAgo timestamp={post.date} />
-            <p>{post.body.substring(0, 100)}</p>
+            <p>{post.body.substring(0, 75)}</p>
+            <p className='postCredit'>
+                <Link to={`post/${post.id}`}>View Post</Link>
+                <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
+            </p>
             <ReactionButtons post={post} />
             <hr />
         </article>
