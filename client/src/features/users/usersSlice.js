0eda673730a3573529858dee2516bc758aef52da
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-05-06 09:13:10 +0200

    extend users

--- client/src/features/users/usersSlice.js
+++ client/src/features/users/usersSlice.js
@@ -1,12 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-    {id: '0', name: 'Dude Lebowski'},
-    {id: '1', name: 'Neil Young'},
-    {id: '2', name: 'Jakub Wilczek'},
-    {id: '3', name: 'Szymon Bączyk'},
-    {id: '4', name: 'Dave Gray'},
-]
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
+
+const initialState = {
+    users: [],
+    status: 'idle', // ildle, loading, succeeded, failed
+    error: null
+}
+
+export const fetchUsers = createAsyncThunk('users/fechUsers', async () => {
+    const response = await axios.get(USERS_URL)
+    return response.data
+})
 
 
 const usersSlice = createSlice({
@@ -14,6 +20,20 @@ const usersSlice = createSlice({
     initialState,
     reducers: {
 
+    },
+    extraReducers(builder){
+        builder
+            .addCase(fetchUsers.pending, (state, action) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchUsers.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                return action.payload
+            })
+            .addCase(fetchUsers.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message
+            })
     }
 })
 
