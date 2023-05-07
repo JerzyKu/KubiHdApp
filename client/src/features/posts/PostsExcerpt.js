9452dea65a4389f78339bc0c69971746895d5c58
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-05-07 10:15:57 +0200

    fix link andadd 3dots

--- client/src/features/posts/PostsExcerpt.js
+++ client/src/features/posts/PostsExcerpt.js
@@ -9,9 +9,9 @@ export default function PostsExcerpt({ post }) {
     return (
         <article>
             <h3>{post.title}</h3>
-            <p>{post.body.substring(0, 75)}</p>
+            <p>{post.body.substring(0, 75)}(...) </p>
             <p className='postCredit'>
-                <Link to={`post/${post.id}`}>View Post</Link>
+                <Link to={`post/${post._id}`}>View Post</Link>
                 <PostAuthor userId={post.userId} />
                 <TimeAgo timestamp={post.date} />
             </p>
