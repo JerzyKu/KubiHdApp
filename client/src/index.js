55b392825f2ca269db8bf3bdff47497d23bc4eae
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2023-05-17 14:32:17 +0200

    important

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { store } from './app/store';
 import { Provider } from 'react-redux';
@@ -15,7 +15,9 @@ root.render(
   // <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter >
-        <App />
+      <Routes>
+        <Route path='/*' element={<App />} />
+      </Routes>
       </BrowserRouter>
     </Provider>
   // </React.StrictMode>
